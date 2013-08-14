cf7f3af96550d20f6db3ce312e7c2fb5e146a15d
coty <sutherland.coty@gmail.com>
2013-08-14 16:04:12 -0400

    Added some more deps as well as application information.

--- setup/setup.py
+++ setup/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup#, find_packages
+from setuptools import setup, find_packages
 
 setup(
-    #name='Your Application',
-    #version='1.0',
+    name='Flask Practice',
+    version='0.1',
     #long_description=__doc__,
-    #packages=find_packages()
-    #include_package_data=True,
-    #zip_safe=False,
-    install_requires=['Flask','HTML.py','pymysql','flask-restful','flask-httpauth']
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask','HTML.py','pymysql','flask-restful','flask-httpauth', 'pyyaml']
 )
